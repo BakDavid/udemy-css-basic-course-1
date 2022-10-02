3db84dacf4fa9918614a8b7d9360b2478701378f
Bak David <bak.david96@gmail.com>
2022-10-02 22:16:50 +0300

    Adding side navigation js logic to open/close

--- shared.js
+++ shared.js
@@ -2,6 +2,9 @@ let backdrop = document.querySelector(".backdrop");
 let modal = document.querySelector(".modal");
 let modalNoButton = document.querySelector(".modal__action--negative");
 let selectPlanButton = document.querySelectorAll(".plan button.button");
+
+let toggleButton = document.querySelector(".toggle-button");
+let mobileNav = document.querySelector("nav.mobile-nav");
 // console.dir(backdrop);
 // console.dir(selectPlanButton);
 for (let i = 0; i < selectPlanButton.length; i++) {
@@ -11,10 +14,18 @@ for (let i = 0; i < selectPlanButton.length; i++) {
   });
 }
 
-backdrop.addEventListener("click", closeModal);
+backdrop.addEventListener("click", function () {
+  mobileNav.style.display = "none";
+  closeModal();
+});
 modalNoButton.addEventListener("click", closeModal);
 
 function closeModal() {
   backdrop.style.display = "none";
   modal.style.display = "none";
 }
+
+toggleButton.addEventListener("click", function () {
+  mobileNav.style.display = "block";
+  backdrop.style.display = "block";
+});
